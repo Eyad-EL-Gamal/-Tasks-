6841bba25d92f67de957cbd9dc1ca16ff0dd65c7
Eyad-EL-Gamal <ahmedeiad666@gmail.com>
2025-08-30 20:59:00 +0300

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,15 @@
+product1 = 50
+product2 = 60
+product3 = 40
+total_budget = 200
+print("product1:", product1)
+print("product2:", product2)
+print("product3:", product3)
+print("total budget:", total_budget)
+total_cost = product1 + product2 + product3
+print("total cost:", total_cost)
+compare = total_budget > total_cost
+print("compare", compare)
+#modulos = total buudget % total cost 
+difference = total_budget % total_cost
+print("difference:", difference)
